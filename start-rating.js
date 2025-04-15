a54c5f056e8bcc61ab77e40f36fc21cda69fbc2b
F1Soda <golik.tima@gmail.com>
2025-04-15 16:08:07 +0500

    Ruslani Tenesini

---
+++ start-rating.js
@@ -0,0 +1,16 @@
+﻿const containers = document.querySelectorAll('.star-container');
+
+containers.forEach((container, idx) => {
+    container.addEventListener('mouseover', () => {
+        for (let i = 0; i <= idx; i++) {
+            containers[i].classList.add('hover');
+        }
+        for (let i = idx + 1; i < containers.length; i++) {
+            containers[i].classList.remove('hover');
+        }
+    });
+
+    container.addEventListener('mouseout', () => {
+        containers.forEach(c => c.classList.remove('hover'));
+    });
+});
